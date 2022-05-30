10df77e0f3c41186acc750dc33ce23de1fe68b21
atheerweb <yousoufmostafamuslim@gmail.com>
2022-05-30 14:41:02 +0200

    changing section by scroll

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,47 @@ const NavBar = () => {
   const [activeNav, setActiveNav] = useState(false);
   const [activeNavMobile, setActiveNavMobile] = useState(false);
   const activate = () => {
+    console.log(window.scrollY);
+    if (window.scrollY < 443) {
+      document.querySelectorAll(".nav__link").forEach((link) => {
+        link.classList.remove("nav__active");
+        link.childNodes[2].childNodes[0].classList.remove("nav__bar__active");
+        link.childNodes[0].childNodes[0].classList.remove("nav__bar__active");
+      });
+      document.querySelector("#nav__0").classList.add("nav__active");
+      document
+        .querySelector("#nav__0")
+        .childNodes[2].childNodes[0].classList.add("nav__bar__active");
+      document
+        .querySelector("#nav__0")
+        .childNodes[0].childNodes[0].classList.add("nav__bar__active");
+    } else if (window.scrollY < 800) {
+      document.querySelectorAll(".nav__link").forEach((link) => {
+        link.classList.remove("nav__active");
+        link.childNodes[2].childNodes[0].classList.remove("nav__bar__active");
+        link.childNodes[0].childNodes[0].classList.remove("nav__bar__active");
+      });
+      document.querySelector("#nav__1").classList.add("nav__active");
+      document
+        .querySelector("#nav__1")
+        .childNodes[2].childNodes[0].classList.add("nav__bar__active");
+      document
+        .querySelector("#nav__1")
+        .childNodes[0].childNodes[0].classList.add("nav__bar__active");
+    } else {
+      document.querySelectorAll(".nav__link").forEach((link) => {
+        link.classList.remove("nav__active");
+        link.childNodes[2].childNodes[0].classList.remove("nav__bar__active");
+        link.childNodes[0].childNodes[0].classList.remove("nav__bar__active");
+      });
+      document.querySelector("#nav__2").classList.add("nav__active");
+      document
+        .querySelector("#nav__2")
+        .childNodes[2].childNodes[0].classList.add("nav__bar__active");
+      document
+        .querySelector("#nav__2")
+        .childNodes[0].childNodes[0].classList.add("nav__bar__active");
+    }
     if (window.scrollY > 0) {
       setActiveNav(true);
     } else {
@@ -43,7 +84,12 @@ const NavBar = () => {
         <div className="button__bar"></div>
       </div>
       <ul id="nav__pc">
-        <li className="nav__link nav__active" onClick={navPc} data="aboutme">
+        <li
+          className="nav__link nav__active"
+          onClick={navPc}
+          data="aboutme"
+          id="nav__0"
+        >
           <div className="nav__bar__wrapper ">
             <div className="nav__bar nav__bar__active"></div>
           </div>
@@ -52,7 +98,12 @@ const NavBar = () => {
             <div className="nav__bar nav__bar__active"></div>
           </div>
         </li>
-        <li className="nav__link" onClick={navPc} data="testimonials">
+        <li
+          className="nav__link"
+          onClick={navPc}
+          data="testimonials"
+          id="nav__1"
+        >
           <div className="nav__bar__wrapper">
             <div className="nav__bar"></div>
           </div>
@@ -61,7 +112,12 @@ const NavBar = () => {
             <div className="nav__bar"></div>
           </div>
         </li>
-        <li className="nav__link" onClick={navPc} data="recentprojects">
+        <li
+          className="nav__link"
+          onClick={navPc}
+          data="recentprojects"
+          id="nav__2"
+        >
           <div className="nav__bar__wrapper">
             <div className="nav__bar"></div>
           </div>
